0059c4ab4898062c51d64dec9ec8651123ea7578
Ashish <ashishkhobragade45@gmail.com>
2018-05-14 12:03:48 +0530

    Add driver layout

--- app/src/main/java/asia/getl/dmc_getl/Home.java
+++ app/src/main/java/asia/getl/dmc_getl/Home.java
@@ -80,7 +80,7 @@ public class Home extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.menu_driver_add) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
